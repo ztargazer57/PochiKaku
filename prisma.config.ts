6dfe6379bfb38c8d76775c2ffa93c1477cb38c83
ztargazer57 <johnmhellsinagpulo@gmail.com>
2026-04-24 13:22:12 +0800

    Smoll Changes

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, env } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
-      seed: 'tsx prisma/userseed.ts',
+      seed: 'tsx prisma/seed.ts',
     },
   datasource: {
     url: env("DATABASE_URL"),
